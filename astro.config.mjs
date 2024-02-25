7f7d2e4bba64b2527dd02a8fc1d022dbe4367f5f
Shaw <shawzgames@gmail.com>
2024-02-25 21:34:28 +0100

    fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
 import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
-import vercel from '@astrojs/vercel/serverless';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(),astroI18next()],
-    output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
+  integrations: [tailwind(), astroI18next()],
+  output: "server",
+  adapter: vercel()
 });
