f7ac3e9a66c54165b4342a4b564b707c98e983c6
Shaw <shawzgames@gmail.com>
2024-02-25 21:29:53 +0100

    try vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(),astroI18next()],
+    output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 });
