f32d63e3f078bc3185ca48ee74ddc26fdf44e475
Shaw <shawzgames@gmail.com>
2024-02-26 22:12:20 +0100

    Remove empty line and comment in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import astroI18next from "astro-i18next";
 import tailwind from "@astrojs/tailwind";
 import vercelStatic from "@astrojs/vercel/static";
 
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), astroI18next()],
   output: "static",
